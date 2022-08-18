ef2fbab30e53e138009403edb3057a6db3c96f53
tarstarcas@gmail.com <ivan.major.888@gmail.com>
2022-08-18 18:26:13 +0300

    home_page

---
+++ components/CardHome.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+.card-home
+	.card-home__icon(
+		v-if="icon"
+	)
+		img(
+			:src="require(`../assets/svg/home/${icon}.svg`)"
+		)
+		span {{ img1 }}
+
+	//- .card-home__img
+	//- 	img(
+	//- 		:src="'../../assets/svg/garage_icon.svg'"
+	//- 	)
+
+	.card-home__block
+		h2.card-home__title {{ name }}
+
+		.card-home__number(
+			v-if="numderOfCars !== null"
+		) {{ numderOfCars }}
+
+		//- .card-home__icon(
+		//- 	v-if="arrow"
+		//- )
+			//- include ../../assets/svg/garage_icon.svg
+</template>
+
+<script>
+export default {
+	props:[
+		"id",
+		"name",
+		"numderOfCars",
+		"icon",
+		"img1",
+		"arrow"
+	],
+
+	name: 'CardHome',
+
+	data() {
+		return {
+		};
+	},
+	methods: {
+
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.card-home {
+	background-color: #4f4f4f;
+	&__icon {
+		width: 40px;
+		height: 40px;
+
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	&__img {
+
+	}
+
+	&__block {
+
+	}
+
+	&__title {
+
+	}
+
+	&__number {
+
+	}
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,62 @@
 <template lang="pug">
+.home
+	nuxt-link.home__link(
+		v-for="card in cards"
+		:key="card.id"
+		:to="'/'"
+	)
+		CardHome(
+			:id="card.id"
+			:name="card.name"
+			:numderOfCars="card.numderOfCars ? card.numderOfCars : null"
+			:icon="card.icon ? card.icon : null"
+			:img1="card.img"
+			:arrow="card.arrow ? card.arrow : null"
+		)
+
 </template>
 
 <script>
+import CardHome from '../components/CardHome';
+
 export default {
-  name: 'IndexPage'
+	name: 'IndexPage',
+	data() {
+		return {
+			cards: [
+				{
+					id: 1,
+					name: 'Garage',
+					numderOfCars: '04',
+					icon: 'garage_icon',
+					img: 'avto_big.png'
+
+				},
+				{
+					id: 2,
+					name: 'NFT Market',
+					icon: 'market_icon',
+					img: 'avto_right.png'
+				},
+				{
+					id: 3,
+					name: 'Finance',
+					icon: 'finance_icon',
+					img: 'finance.png'
+				},
+				{
+					id: 4,
+					name: 'Start Race',
+					img: 'avto_big.png',
+					arrow: 'arrow_right.svg'
+				},
+			]
+		}
+	},
+
+	components: {
+		CardHome,
+	}
+
 }
 </script>
