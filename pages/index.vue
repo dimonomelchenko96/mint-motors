e26e0da0ecb8773742ca3903cef6ecace9911d24
tarstarcas@gmail.com <ivan.major.888@gmail.com>
2022-08-19 14:23:28 +0300

    19.08.2022

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,20 @@
 <template lang="pug">
 .home
-	nuxt-link.home__link(
-		v-for="card in cards"
-		:key="card.id"
-		:to="'/'"
-	)
-		CardHome(
-			:id="card.id"
-			:name="card.name"
-			:numderOfCars="card.numderOfCars ? card.numderOfCars : null"
-			:icon="card.icon ? card.icon : null"
-			:image="card.image"
-			:arrow="card.arrow ? card.arrow : null"
+	.home__container
+		.home__link(
+			v-for="card in cards"
+			:key="card.id"
+			:class="'home__link_position-' + card.param.blockPosition"
 		)
+			CardHome(
+				:name="card.name"
+				:numderOfCars="card.numderOfCars"
+				:icon="card.icon"
+				:image="card.image"
+				:arrow="card.arrow"
+				:param="card.param"
+				:link="card.link"
+			)
 </template>
 
 <script>
@@ -26,31 +28,57 @@ export default {
             cards: [
                 {
                     id: 1,
+					link: 'garage',
                     name: 'Garage',
                     numderOfCars: '04',
                     icon: 'garage_icon',
                     image: 'avto_big',
                     param: {
-                        bacckground: 'black',
+                        background: 'black',
+						textColor: 'yellow',
+						imgPosition: 'center',
+						blockPosition: 'first',
                     },
                 },
                 {
                     id: 2,
+					link: 'market',
                     name: 'NFT Market',
                     icon: 'market_icon',
                     image: 'avto_right',
+					param: {
+                        background: 'yellow',
+						textColor: 'gray',
+						imgPosition: 'right-bottom',
+						blockPosition: 'second',
+						textTransfer: true,
+                    },
                 },
                 {
                     id: 3,
+					link: 'finance',
                     name: 'Finance',
                     icon: 'finance_icon',
                     image: 'finance',
+					param: {
+                        background: 'turquoise',
+						textColor: 'black',
+						imgPosition: 'center',
+						blockPosition: 'third',
+                    },
                 },
                 {
                     id: 4,
+					link: 'racing',
                     name: 'Start Race',
                     image: 'race',
                     arrow: 'arrow_right',
+					param: {
+                        background: 'white',
+						textColor: 'black',
+						imgPosition: 'center',
+						blockPosition: 'fourth',
+                    },
                 },
             ],
         };
@@ -62,17 +90,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// .div {
-// 	display: flex;
-// 	height: 100vh;
-// 	jc
-// }
 .home {
     display: flex;
-    margin: 0 auto;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    height: d(616);
+	height: 100vh;
+
+	&__container {
+		margin: auto;
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-rows: repeat(2, 1fr);
+
+		grid-template-areas:
+			"first first second third"
+			"first first second fourth";
+
+		gap: d(20);
+
+		height: d(457);
+		width: d(1200);
+	}
+
+	&__link {
+		display: inline-block;
+		&_position {
+			&-first {
+				grid-area: first;
+			}
+
+			&-second {
+				grid-area: second;
+			}
+
+			&-third {
+				grid-area: third;
+			}
+
+			&-fourth {
+				grid-area: fourth;
+			}
+		}
+	}
 }
 </style>
