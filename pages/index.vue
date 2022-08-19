256bf2ccbb4a6b7c5e4e1f96e8c2808b7cbf4cd2
Ihor Perepelytsia <igorekperepelitsa@gmail.com>
2022-08-19 10:41:10 +0300

    v123

--- pages/index.vue
+++ pages/index.vue
@@ -16,49 +16,46 @@
 </template>
 
 <script>
-import CardHome from '../components/CardHome';
+import CardHome from '~/components/CardHome';
 
 export default {
-<<<<<<< HEAD
-	name: 'IndexPage',
-	data() {
-		return {
-			cards: [
-				{
-					id: 1,
-					name: 'Garage',
-					numderOfCars: '04',
-					icon: 'garage_icon',
-					image: 'avto_big'
+    name: 'IndexPage',
+    data() {
+        return {
+            cards: [
+                {
+                    id: 1,
+                    name: 'Garage',
+                    numderOfCars: '04',
+                    icon: 'garage_icon',
+                    image: 'avto_big',
+                },
+                {
+                    id: 2,
+                    name: 'NFT Market',
+                    icon: 'market_icon',
+                    image: 'avto_right',
+                },
+                {
+                    id: 3,
+                    name: 'Finance',
+                    icon: 'finance_icon',
+                    image: 'finance',
+                },
+                {
+                    id: 4,
+                    name: 'Start Race',
+                    image: 'race',
+                    arrow: 'arrow_right',
+                },
+            ],
+        };
+    },
 
-				},
-				{
-					id: 2,
-					name: 'NFT Market',
-					icon: 'market_icon',
-					image: 'avto_right'
-				},
-				{
-					id: 3,
-					name: 'Finance',
-					icon: 'finance_icon',
-					image: 'finance'
-				},
-				{
-					id: 4,
-					name: 'Start Race',
-					image: 'race',
-					arrow: 'arrow_right'
-				},
-			]
-		}
-	},
-
-	components: {
-		CardHome,
-	}
-
-}
+    components: {
+        CardHome,
+    },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -68,11 +65,11 @@ export default {
 // 	jc
 // }
 .home {
-	display: flex;
-	margin: 0 auto;
-	position: absolute;
-	top: 50%;
-	transform: translateY(-50%);
-	height: d(616);
+    display: flex;
+    margin: 0 auto;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    height: d(616);
 }
 </style>
